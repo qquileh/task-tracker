1c04d787435583b302c02d2fe8961f1f92dad639
Melis T <mels700mels700@gmail.com>
2025-03-05 17:22:57 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // Ваш код
+    for (int i = 0; i < argc; ++i) {
+        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+    }
+    return 0;
+}
