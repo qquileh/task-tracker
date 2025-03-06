e44db24ea8ace34cffa1fc60573880f17eb94f94
Melis T <mels700mels700@gmail.com>
2025-03-06 14:41:26 +0300

    CLArguments

--- src/CLArgs.cpp
+++ src/CLArgs.cpp
@@ -1,10 +1,50 @@
 #include "../include/CLArgs.h"
 
-CommandLineArguments::CommandLineArguments(int argc, char* argv[]) {
-	if (argc > 1) {
-		this->command = argv[1];
-		for (size_t i = 2; i != argc; ++i) {
-			this->arguments.push_back(argv[i]);
-		}
-	}
+
+void CommandLineArguments::parseCommand(const std::string& cmd) {
+    if (cmd == "add") {
+        _command = Command::Add;
+    } else if (cmd == "update") {
+        _command = Command::Update;
+    } else if (cmd == "delete") {
+        _command = Command::Delete;
+    } else if (cmd == "mark-in-progress") {
+        _command = Command::MarkInProgress;
+    } else if (cmd == "mark-done") {
+        _command = Command::MarkDone;
+    } else if (cmd == "list") {
+        _command = Command::List;
+    } else {
+        throw std::invalid_argument("Invalid command: " + cmd);
+    }
 }
+
+void CommandLineArguments::parseListSubcommand(const std::string& subcmd) {
+    if (subcmd == "done") {
+        _command = Command::ListDone;
+    } else if (subcmd == "todo") {
+        _command = Command::ListTodo;
+    } else if (subcmd == "in-progress") {
+        _command = Command::ListInProgress;
+    }
+}
+
+void CommandLineArguments::parseArguments(int argc, char* argv[], size_t start) {
+    for (size_t i = start; i < argc; ++i) {
+        _arguments.push_back(argv[i]);
+    }
+}
+
+CommandLineArguments::CommandLineArguments(int argc, char* argv[]) {
+    if (argc > 1) {
+        parseCommand(argv[1]);
+        if (_command == Command::List && argc > 2) {
+            parseListSubcommand(argv[2]);
+            _argStartNum = 3;
+        }
+        parseArguments(argc, argv, _argStartNum);
+    } else {
+        throw std::invalid_argument("No command provided!");
+    }
+}
+
